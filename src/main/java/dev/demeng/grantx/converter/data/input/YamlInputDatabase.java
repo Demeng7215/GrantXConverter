e9a66deff6b6084a1600c7b98f1b061380827090
Demeng <me@demeng.dev>
2020-10-01 20:15:10 -0400

    Finish converter for YAML input

--- src/main/java/dev/demeng/grantx/converter/data/input/YamlInputDatabase.java
+++ src/main/java/dev/demeng/grantx/converter/data/input/YamlInputDatabase.java
@@ -1,12 +1,16 @@
 package dev.demeng.grantx.converter.data.input;
 
 import dev.demeng.grantx.converter.model.Grant;
+import org.simpleyaml.configuration.ConfigurationSection;
 import org.simpleyaml.configuration.file.YamlFile;
 import org.simpleyaml.exceptions.InvalidConfigurationException;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 
 public class YamlInputDatabase implements InputDatabase {
 
@@ -27,6 +31,39 @@ public class YamlInputDatabase implements InputDatabase {
 
   @Override
   public List<Grant> getGrants() {
-    return null;
+
+    final List<Grant> grants = new ArrayList<>();
+
+    final ConfigurationSection section = data.getConfigurationSection("grants");
+    Objects.requireNonNull(section);
+
+    for (String key : section.getKeys(false)) {
+
+      final int id = Integer.parseInt(key);
+      final Grant.Status status = Grant.Status.valueOf(section.getString(key + ".status"));
+      final long time = section.getLong(key + ".time");
+      final String target = section.getString(key + ".target");
+      final String staff = section.getString(key + ".staff");
+      final String rank = section.getString(key + ".rank");
+      final String duration = section.getString(key + ".duration");
+      final String reason = section.getString(key + ".reason");
+      final String revoker = section.getString(key + ".revoker");
+
+      grants.add(
+          new Grant(
+              id,
+              status,
+              time,
+              UUID.fromString(target),
+              UUID.fromString(staff),
+              revoker.equals("none") ? null : UUID.fromString(revoker),
+              rank,
+              "global",
+              duration,
+              reason,
+              status == Grant.Status.REVOKED ? 0 : -1));
+    }
+
+    return grants;
   }
 }
--- src/main/java/dev/demeng/grantx/converter/model/Grant.java
+++ src/main/java/dev/demeng/grantx/converter/model/Grant.java
@@ -1,4 +1,28 @@
 package dev.demeng.grantx.converter.model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
 public class Grant {
+
+  @Getter private final int id;
+  @Getter private final Status status;
+  @Getter private final long time;
+  @Getter private final UUID target;
+  @Getter private final UUID issuer;
+  @Getter private final UUID revoker;
+  @Getter private final String rank;
+  @Getter private final String server;
+  @Getter private final String duration;
+  @Getter private final String reason;
+  @Getter private final long revokeTime;
+
+  public enum Status {
+    ACTIVE,
+    EXPIRED,
+    REVOKED
+  }
 }
